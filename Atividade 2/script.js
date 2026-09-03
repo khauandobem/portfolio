18a3b85469822250098253e4142d1cd38a9769f3
khauandobem <khauan.assis@escola.pr.gov.br>
2026-09-03 08:03:49 -0300

    Add files via upload

---
+++ Atividade 2/script.js
@@ -0,0 +1,10 @@
+function ola() {
+alert("Você clicou em mim >.<")
+};
+function tchau() {
+    alert("Você tem problema?")
+};
+function aleatorio() {
+    let numeroAleatorio = Math.random();
+    alert("O número é" + numeroAleatorio);
+}
